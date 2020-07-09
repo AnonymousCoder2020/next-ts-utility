30de948ffbe1019437a2cb7d8c8216e4b605f2e8
AnonymousCoder2020 <virtual.creeper.ver1.3.2@gmail.com>
2020-07-09 12:25:35 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+export const condProp = <B extends boolean, T extends object>(bool: B, obj: T) => (bool ? obj : {}) as B extends true ? T : {}
+
+type condPropsObject<T> = {
+  [P in keyof T]: [boolean, T[P]]
+}
+
+export const condProps = <T>(obj: condPropsObject<T>) => {
+  return Object.fromEntries(
+    (Object.entries(obj) as [keyof T, [boolean, T[keyof T]]][]).filter(([_, [bool]]) => bool).map(([key, [_, val]]) => [key, val])
+  ) as { [P in keyof T]?: T[P] }
+}
+
+export const notUndefinedPropsAnd = <P extends number | string, T extends { [K in P]?: any }>(
+  obj: T,
+  props: P[]
+): obj is T & { [K in P]: Exclude<T[K], undefined> } => {
+  return props.every(propName => obj[propName] !== undefined)
+}
+
+// 再考の余地あり
+export const instanceAnd = <A>(value: unknown, instances: { new (): A }[]): value is A => {
+  return instances.every(instance => value instanceof instance)
+}
+
+export const instanceOr = <A>(value: unknown, instances: { new (): A }[]): value is A => {
+  return instances.some(instance => value instanceof instance)
+}
