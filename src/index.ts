edc613b9aa04ae712be17431cdfffe17dd34c6b1
AnonymousCoder2020 <virtual.creeper.ver1.3.2@gmail.com>
2020-07-11 15:01:28 +0900

    add mergePlainObject

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import { DeepPartial } from 'ts-essentials'
+import { mergeWith, isPlainObject } from 'lodash'
+
 type condPropsObject<T> = {
   [P in keyof T]: [boolean, T[P]]
 }
@@ -22,6 +25,12 @@ export const instanceAnd = <A>(value: unknown, instances: { new (): A }[]): valu
 
 export const instanceOr = <A>(value: unknown, instances: { new (): A }[]): value is A => {
   return instances.some(instance => value instanceof instance)
+}
+
+export const mergePlainObject = <T extends object>(merge: DeepPartial<T>, base: T) => {
+  return mergeWith(merge, base, (obj: any) => {
+    return isPlainObject(obj) ? undefined : obj
+  })
 }
 
 export * from './dom'
