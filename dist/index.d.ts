a1b39a9c5498fa32a693c8e255194847d3cfee48
AnonymousCoder2020 <virtual.creeper.ver1.3.2@gmail.com>
2020-07-14 14:15:00 +0900

    Spell miss

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,7 @@
 export { default as synthesizeDOMRect } from './synthesizeDomRect';
 export { default as condProps } from './condProps';
 export { default as mergePlainObject } from './mergePlainObject';
-export { default as forEarchMulti } from './forEachMulti';
+export { default as forEachMulti } from './forEachMulti';
 export { default as findResult } from './findResult';
 export { default as notUndefinedPropsAnd } from './notUndefinedPropsAnd';
 export { default as instanceAnd } from './instanceAnd';
