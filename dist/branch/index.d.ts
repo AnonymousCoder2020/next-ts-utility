0223d07dc22d533112a697aad679bec358a022d5
AnonymousCoder2020 <virtual.creeper.ver1.3.2@gmail.com>
2020-08-28 13:47:20 +0900

    Change branch

--- dist/branch/index.d.ts
+++ dist/branch/index.d.ts
@@ -1,3 +1,7 @@
-declare type InlineBranch = [boolean, Function];
-declare const _default: <I extends InlineBranch>(branches: I[]) => any;
+declare type KeyofNumber<T> = Extract<keyof T, number>;
+declare type InlineBranch = readonly [boolean, () => any];
+declare type ReturnTypeBranch<I extends readonly InlineBranch[]> = {
+    [P in KeyofNumber<I>]?: ReturnType<I[P][1]>;
+};
+declare const _default: <I extends readonly InlineBranch[]>(branches: I) => ReturnTypeBranch<I>[Extract<keyof I, number>];
 export default _default;
